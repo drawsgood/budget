6c0fed1f8dcddf21b70e8cc3dde737cdd1d9ecc2
Michael Myers Jr <mbmyers@gmail.com>
2017-11-21 20:13:22 -0600

    Initial Commit

--- My_Script.js
+++ My_Script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 					$('#list').append("<tr class='myBorderColor'><td style='border: none;' class='myBGColor'>$"+value.amount+"</td><td style='border: none; text-align: right' class='myBGColor'>"+value.category+"</td>");
 				});
 
-				$('#total').append("<h1 style='font-size: 6em; font-weight: 600;' class='text-center'>$"+myVal.fart.toFixed(2)+"</h1>");
+				$('#total').append("<h1 style='font-size: 5em; font-weight: 600;' class='text-center'>$"+myVal.fart.toFixed(2)+"</h1>");
 				console.log(myVal.fart.toFixed(2));
 
 				console.log(finalNum.toFixed(2));
